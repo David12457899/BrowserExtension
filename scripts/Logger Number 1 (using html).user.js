62162519108385a7306b19be24c6ec3cc97e257f
David Tsitsiashvili <david12457899@gmail.com>
2022-01-26 20:46:59 +0200

    Added first 2 working scripts and a picture examplem of how they work

---
+++ scripts/Logger Number 1 (using html).user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Logger Number 1 (using html)
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Logs the username when logging into AWS
+// @author       david12457899
+// @match        aws.amazon.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @grant        none
+// ==/UserScript==
+
+function findUserName() {
+  return customerLogin;
+}
+
+(function() {
+    'use strict';
+
+    console.log("Your user: " + findUserName());
+})();
---
+++ scripts/Logger Number 2 (using cookies).user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         Logger Number 2 (using cookies)
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Logs the username when logging into AWS
+// @author       david12457899
+// @match        aws.amazon.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @grant        none
+// ==/UserScript==
+
+function findUserType(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return decodeURI(parts.pop().split(';').shift()).split("%2C")[2].split('"')[3]
+}
+
+(function() {
+    'use strict';
+
+    console.log("Found User: " + findUserType("aws-userInfo"));
+})();
